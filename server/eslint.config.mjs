b4e8db7c22cf440054e27938eaeac1d908dfd5ec
illakoroliuk <illakoroluk62@gmail.com>
2025-07-01 01:10:15 +0300

    setup eslint for server

---
+++ server/eslint.config.mjs
@@ -0,0 +1,35 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettier from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+
+      "@typescript-eslint/explicit-function-return-type": ["error"],
+      "@typescript-eslint/no-explicit-any": ["error"],
+      "@typescript-eslint/no-empty-function": ["error"],
+
+      "no-fallthrough": "error",
+      "no-console": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+    },
+  },
+  {
+    ...prettier,
+    files: ["**/*.ts"],
+  },
+];
