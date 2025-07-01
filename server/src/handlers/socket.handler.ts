447a449a166bb0d1b1f454c9ba8d2751106c0a63
illakoroliuk <illakoroluk62@gmail.com>
2025-07-01 20:31:13 +0300

    modify socket.handler for logger

--- server/src/handlers/socket.handler.ts
+++ server/src/handlers/socket.handler.ts
@@ -3,6 +3,9 @@ import { Server, Socket } from "socket.io";
 import { ListEvent } from "../common/enums/enums";
 import { Database } from "../data/database";
 import { ReorderService } from "../services/reorder.service";
+import { LoggerPublisher } from "../services/logger-publisher";
+import { FileLogger } from "../services/file-logger";
+import { ConsoleErrorLogger } from "../services/console-error-logger";
 
 abstract class SocketHandler {
   protected db: Database;
@@ -11,10 +14,18 @@ abstract class SocketHandler {
 
   protected io: Server;
 
+  protected logger: LoggerPublisher;
+
   public constructor(io: Server, db: Database, reorderService: ReorderService) {
     this.io = io;
     this.db = db;
     this.reorderService = reorderService;
+
+    // PATTERN:Observer
+
+    this.logger = new LoggerPublisher();
+    this.logger.subscribe(new FileLogger("./app.log"));
+    this.logger.subscribe(new ConsoleErrorLogger());
   }
 
   public abstract handleConnection(socket: Socket): void;
