64298f92be8714e3ca8a8909a4c952462bf91ee0
illakoroliuk <illakoroluk62@gmail.com>
2025-07-01 19:33:04 +0300

    refact code structuree using prettier

--- server/src/assets/mock-data.ts
+++ server/src/assets/mock-data.ts
@@ -1,35 +1,39 @@
-import { Card } from '../data/models/card';
-import { List } from '../data/models/list';
+import { Card } from "../data/models/card";
+import { List } from "../data/models/list";
 
 const toDo = new List("Backlog");
-toDo.cards = [  new Card(
+toDo.cards = [
+  new Card(
     "Implement list renaming functionality",
-    "Expected result - ability to modify the list name"
-  ),  new Card(
+    "Expected result - ability to modify the list name",
+  ),
+  new Card(
     "Implement card creation feature",
-    "Expected result - ability to add new cards to lists"
-  ),  new Card(
+    "Expected result - ability to add new cards to lists",
+  ),
+  new Card(
     "Implement card deletion functionality",
-    "Expected result - ability to remove cards when delete button is pressed"
+    "Expected result - ability to remove cards when delete button is pressed",
   ),
   new Card(
     "Implement card title renaming",
-    "Expected result - possibility to change the card title"
+    "Expected result - possibility to change the card title",
   ),
   new Card(
     "Implement card description renaming",
-    "Expected result - possibility to change the card description"
-  ),  new Card(
+    "Expected result - possibility to change the card description",
+  ),
+  new Card(
     "Implement task duplication feature",
-    'Expected result - ability to duplicate cards. Should be implemented using Prototype pattern. New ID should be generated for copied card. The card name should include "duplicate" suffix'
+    'Expected result - ability to duplicate cards. Should be implemented using Prototype pattern. New ID should be generated for copied card. The card name should include "duplicate" suffix',
   ),
   new Card(
     "Implement logging on server side",
-    "Expected result - implemented logging with 3 levels: info, warn, error. Should be implemented using Observer pattern. There should be 2 loggers: first will write only errors into console, second will write all logs into file"
+    "Expected result - implemented logging with 3 levels: info, warn, error. Should be implemented using Observer pattern. There should be 2 loggers: first will write only errors into console, second will write all logs into file",
   ),
   new Card(
     "Implement logging of reorder action",
-    "Expected result - implemented logging for the ReorderService (logging proxy). Should be implemented using Proxy pattern. Should be logged for each card/list with the info when it was moved"
+    "Expected result - implemented logging for the ReorderService (logging proxy). Should be implemented using Proxy pattern. Should be logged for each card/list with the info when it was moved",
   ),
 ];
 
@@ -37,7 +41,7 @@ const inProgress = new List("Development");
 inProgress.cards = [
   new Card(
     "Implement list creation functionality",
-    "Expected result - ability to add new lists to the board"
+    "Expected result - ability to add new lists to the board",
   ),
 ];
 
